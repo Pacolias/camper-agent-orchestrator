6a5778a1943b36b78a8e42031798c692a842b5ef
Pacolias <pacomolinac2003@gmail.com>
2026-09-05 19:32:59 +0200

    feat: Update rag_agent_node to utilize HuggingFaceEmbeddings for legal context retrieval

--- app/agents/rag_agent.py
+++ app/agents/rag_agent.py
@@ -1,4 +1,6 @@
 from app.agents.state import RouteState
+from app.core.config import settings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 def rag_agent_node(state: RouteState):
     """
@@ -7,19 +9,29 @@ def rag_agent_node(state: RouteState):
     """
     destination = state.get("destination", "")
 
-    # embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    # vector_store = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
-    # docs = vector_store.similarity_search(
-    #     f"Motorhome and campervan pernoctation normative un {destination}"
-    #     k=3
-    # )
-    # legal_context = "\n".join([doc.page_content for doc in docs])
+    try:
+        embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
-    # Development mock
-    legal_context = (
-        f"Legal context for {destination}: parking allowed "
-        "respecting the vehicle perimeter. It's forbidden to deploy "
-        "elements like awnings or chairs in the public thoroughfare (Traffic Law)."
-    )
+        vector_store = Chroma(
+            persist_directory="./chroma_db", 
+            embedding_function=embeddings
+        )
 
-    return {"legal_context" : legal_context}
+        docs = vector_store.similarity_search(
+            f"Motorhome and campervan pernoctation normative in {destination}"
+            k=3
+        )
+
+        if docs:
+            legal_context = "\n".join([doc.page_content for doc in docs])
+        else:
+            raise ValueError("No coincidences in the data base.")
+
+    except Exception as e:
+        legal_context = (
+            f"Legal context for {destination} (Fallback): parking allowed "
+            "respecting the vehicle perimeter. It's forbidden to deploy "
+            "elements like awnings or chairs in the public thoroughfare (Traffic Law)."
+        )
+
+    return {"legal_context": legal_context}
